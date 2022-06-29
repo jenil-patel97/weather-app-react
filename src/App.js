a70c6539d9627ac6ff5ef619146a01963e6e68c6
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-06-29 11:01:33 +0530

    Added tha data I want

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
-import axios from "axios";
 
 function App() {
-  return <div className="App">
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=bce8c954081c279ac8cacfa24c6cf82f`;
 
-  </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Delhi</p>
+          </div>
+          <div className="temp">
+            <h1>95°F</h1>
+          </div>
+          <div className="discription">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>90°F</p>
+          </div>
+          <div className="humidity">
+            <p>37%</p>
+          </div>
+          <div className="wind">
+            9 MPH
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
